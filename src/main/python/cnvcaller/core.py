df292d1c3058a752a776be8baac0d0827198ca00
Robert Warmerdam <robert_warmerdam@hotmail.com>
2022-04-20 14:00:03 +0200

    Completed proper fit / call methodology for intensity correction

--- src/main/python/cnvcaller/core.py
+++ src/main/python/cnvcaller/core.py
@@ -582,7 +582,6 @@ class IntensityCorrection:
             fit_intercept=regression_fit_intercept)
         self._standardize_scaler = sklearn.preprocessing.StandardScaler()
         self._fitted_reference_variants = None
-        self._corrected = None
         self._pca_fit = None
         self._target_variant_means = None
         self._pca_explained_variance_dataframe = None
@@ -615,15 +614,16 @@ class IntensityCorrection:
         # The residuals can be used in further analyses.
         target_intensity_data_sliced = target_intensity_data.loc[
                                        :, self._target_variants]
-        self._target_variant_means = target_intensity_data_sliced.mean(axis=1)
-        target_intensity_data_sliced = (target_intensity_data_sliced.T - self._target_variant_means).T
+        target_intensity_data_preprocessed = (sklearn.preprocessing.StandardScaler(with_std=False)
+            .fit_transform(
+            target_intensity_data_sliced))
         # Fit the correction model
         self._correction_model.fit(
-            batch_effects, target_intensity_data_sliced)
+            batch_effects, target_intensity_data_preprocessed)
         # Write intensities of locus of interest corrected for batch effects.
-        self._corrected = self._correct_batch_effects(
-            target_intensity_data_sliced, batch_effects)
-        return self._corrected
+        intensities_corrected = self._correct_batch_effects(
+            target_intensity_data_preprocessed, batch_effects)
+        return intensities_corrected
 
     def correct_intensities(self, reference_intensity_data, target_intensity_data):
         # First prepare the reference intensity data.
